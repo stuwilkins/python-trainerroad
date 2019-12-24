ff3d0f5b886a74446443dc251c4b5370829ece94
Stuart B. Wilkins <swilkins@bnl.gov>
2019-12-24 13:17:37 +0000

    Added logging

--- trainerroad/api.py
+++ trainerroad/api.py
@@ -3,6 +3,9 @@ import lxml.html
 from lxml import etree
 from io import StringIO, BytesIO
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 class TrainerRoad:
     _ftp = 'Ftp'
@@ -22,26 +25,32 @@ class TrainerRoad:
 
     def connect(self):
         self._session = requests.Session()
+        self._session.auth = (self._username, self._password)
 
         data = {'Username': self._username,
                 'Password': self._password}
 
-        r = self._session.post(self._login_url, data=data)
+        r = self._session.post(self._login_url, data=data,
+                               allow_redirects=False)
 
-        if r.status_code != 200:
+        if (r.status_code != 200) and (r.status_code != 302):
             # There was an error
-            raise RuntimeError("Error loging in to TrainerRoad")
+            raise RuntimeError("Error loging in to TrainerRoad (Code {})"
+                               .format(r.status_code))
+
+        logger.info('Logged into TrainerRoad as "{}"'.format(self._username))
 
     def disconnect(self):
-        r = self._session.get(self._logout_url)
-        if r.status_code != 200:
-            raise RuntimeError("Error loging out of TrainerRoad")
+        r = self._session.get(self._logout_url, allow_redirects=False)
+        if (r.status_code != 200) and (r.status_code != 302):
+            raise RuntimeError("Error loging out of TrainerRoad (Code {})"
+                               .format(r.status_code))
 
         self._session = None
+        logger.info('Logged out of TrainerRoad as "{}"'.format(self._username))
 
     def __enter__(self):
         self.connect()
-
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
@@ -51,6 +60,7 @@ class TrainerRoad:
         rtn = tree.xpath('//form//input[@name="{}"]/@value'.format(name))
         if not rtn:
             raise RuntimeError('Input {} not found in form'.format(name))
+
         return rtn[0]
 
     def _parse_name(self, tree, name):
@@ -58,6 +68,7 @@ class TrainerRoad:
                          '[@selected="selected"]/@value'.format(name))
         if not rtn:
             raise RuntimeError('Input {} not found in form'.format(name))
+
         return rtn[0]
 
     def _get(self, url):
@@ -65,8 +76,10 @@ class TrainerRoad:
             raise RuntimeError('Not Connected')
 
         r = self._session.get(url)
+
         if r.status_code != 200:
-            raise RuntimeError("Error getting info from TrainerRoad")
+            raise RuntimeError("Error getting info from TrainerRoad (Code {})"
+                               .format(r.status_code))
 
         return r
 
@@ -75,8 +88,10 @@ class TrainerRoad:
             raise RuntimeError('Not Connected')
 
         r = self._session.post(url, data)
+
         if r.status_code != 200:
-            raise RuntimeError("Error posting info to TrainerRoad")
+            raise RuntimeError("Error posting info to TrainerRoad (Code {})"
+                               .format(r.status_code))
 
         return r
 
@@ -101,6 +116,9 @@ class TrainerRoad:
         # Read values
         data, token = self._read_profile()
 
+        logger.info("Read profile values {}".format(data))
+        logger.debug("Token = {}".format(token))
+
         # Update values with new_values
         for key, value in new_values.items():
             if key not in data:
@@ -108,12 +126,16 @@ class TrainerRoad:
                                  .format(key))
             data[key] = str(value)
 
+        logger.info("New profile values {}".format(data))
+
         # Now post the form
         token = {self._rvt: token}
         self._post(self._rider_url, data=dict(**data, **token))
 
         # Now re-read to check
         _data, token = self._read_profile()
+
+        logger.info("Read profile values (verification) {}".format(data))
 
         if data == _data:
             return
